424e3231b24d7a8a8c3a952536384ee01d85016d
Jeongsoo Park <toracle@gmail.com>
2017-05-08 00:21:19 +0900

    change author email

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description='A CLI tool for bothub.studio service',
     long_description=LONG_DESCRIPTION,
     author='Jeongsoo Park',
-    author_email='toracle@gmail.com',
+    author_email='jeongsoo.park@bothub.studio',
     url='https://github.com/bothub-studio/bothub-cli',
     license="Apache 2.0",
     packages=[
